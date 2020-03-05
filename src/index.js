7c91bfaa72faeb7566a01474780fa1194f85808f
mustafe <mustafes900@hotmail.com>
2020-03-05 00:09:26 +0000

    added the initial code for the youtube list display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import {fromEvent} from "rxjs";
-import { filter,map,debounceTime, distinctUntilChanged } from "rxjs/operators";
+import {fromEvent, of} from "rxjs";
+import {fromFetch} from "rxjs/fetch";
+
+import { filter,map,debounceTime, distinctUntilChanged, switchMap, catchError } from "rxjs/operators";
 
 
 const searchInput = document.querySelector("input");
@@ -11,3 +13,29 @@ input.pipe(filter(e => e.target.value.length > 2),
       debounceTime(500),
       distinctUntilChanged())
       .subscribe(x => console.log(x) );
+
+
+//TODO: investigate a way to not share youtube key in github. 
+var apiObservable =  fromFetch("Youtube Url").pipe(
+      switchMap(
+            Response => {
+                  if(Response.ok){
+                        //cool 200
+                        return Response.json(); //cast this into an object that we want
+                  }
+                  else{
+                        //server error
+                        return of({error : true, message: `Error: ${Response.status}`});
+
+                        };
+                  }
+            
+      ),
+      catchError(err => {
+            //network or other client error
+            console.log(err);
+            return of({error : true, message: `Error ${err.message}`});
+      })
+);
+
+

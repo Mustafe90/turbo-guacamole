217bcadb882810d14cfb437e868d22089e5244b6
mustafe <mustafes900@hotmail.com>
2020-03-07 22:49:56 +0000

    added the initial data structure for the data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
-import {fromEvent, of} from "rxjs";
+import {fromEvent, of,from} from "rxjs";
 import {fromFetch} from "rxjs/fetch";
 import {mergeMap} from "rxjs/operators";
 
-import { filter,map,debounceTime, distinctUntilChanged, merge,switchMap, catchError } from "rxjs/operators";
+import { filter,map,debounceTime, distinctUntilChanged,switchMap, catchError } from "rxjs/operators";
 
 
 const searchInput = document.querySelector("input");
 
 let input = fromEvent(searchInput,"keyup");
 
+let videos = from([]);
+
+class Product{};
+
 input.pipe(filter(e => e.target.value.length > 2),
       map(e => e.target.value),
       debounceTime(600),
       distinctUntilChanged(),
+      //TODO: Understand what mergeMap/SwitchMap/CatchError deeper
       mergeMap(query =>  fromFetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyCTH6Zu-mG4DtZ73JYtz0OH9uT5eLh-73w`)),
       switchMap(
-       Response => Response.ok ?  Response.json() :  of({error : true, message: `Error: ${Response.status}`})            
+       Response => Response.ok ? from(Response.json())  :  of({error : true, message: `Error: ${Response.status}`})            
       ),
+      map(data => {
+            //TODO: Errors will stop the observable
+            const {etag,
+                   regionCode,
+                   nextPageToken,
+                   pageInfo: {totalResults,resultsPerPage},
+                   items} = data;
+            return {
+                  etag,
+                  regionCode,
+                  nextPageToken,
+                  pageInfo: {
+                        totalResults,
+                        resultsPerPage
+                  },
+                  items
+            };
+      }), 
       catchError(err => {
             //network or other client error
             console.log(err);
@@ -25,11 +48,9 @@ input.pipe(filter(e => e.target.value.length > 2),
       .subscribe({
             next: response => console.log(response),
             error: error => console.log(error),
-            complete: complete => console.log(complete)
+            complete: complete => console.log("Compelet")
       });
    
-
-
 //TODO: Construct this string yourself 
 
 

3778755de44c06f92fa154554c68a998ebd9defe
mustafe <mustafes900@hotmail.com>
2020-03-07 22:13:48 +0000

    completed the seach for youtube vidoes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import {fromEvent, of} from "rxjs";
 import {fromFetch} from "rxjs/fetch";
-import {flatMap} from "rxjs/operators";
+import {mergeMap} from "rxjs/operators";
 
-import { filter,map,debounceTime, distinctUntilChanged, switchMap, catchError } from "rxjs/operators";
+import { filter,map,debounceTime, distinctUntilChanged, merge,switchMap, catchError } from "rxjs/operators";
 
 
 const searchInput = document.querySelector("input");
@@ -11,39 +11,25 @@ let input = fromEvent(searchInput,"keyup");
 
 input.pipe(filter(e => e.target.value.length > 2),
       map(e => e.target.value),
-      debounceTime(500),
-      distinctUntilChanged(), 
-      fromFetch(x => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${x}}&key=AIzaSyCTH6Zu-mG4DtZ73JYtz0OH9uT5eLh-73w`),
-      switchMap(Success,Error))
-      .subscribe(x => console.log(x));
-
-
-//TODO: investigate a way to not share youtube key in github. 
-
-//TODO: Construct this string yourself. 
-
-function Error(err){
-     return catchError(err => {
+      debounceTime(600),
+      distinctUntilChanged(),
+      mergeMap(query =>  fromFetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyCTH6Zu-mG4DtZ73JYtz0OH9uT5eLh-73w`)),
+      switchMap(
+       Response => Response.ok ?  Response.json() :  of({error : true, message: `Error: ${Response.status}`})            
+      ),
+      catchError(err => {
             //network or other client error
             console.log(err);
             return of({error : true, message: `Error ${err.message}`});
-      })
-}
-function Success (Response) {
+      }))  
+      .subscribe({
+            next: response => console.log(response),
+            error: error => console.log(error),
+            complete: complete => console.log(complete)
+      });
+   
 
-      return(Response) => {
-                        if(Response.ok){
-                              //cool 200
-                              return Response.json(); //cast this into an object that we want
-                        }
-                        else{
-                              //server error
-                              return of({error : true, message: `Error: ${Response.status}`});
-      
-                              };
-                        };
-                        
-}
 
+//TODO: Construct this string yourself 
 
 

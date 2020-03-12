9ff570531a24c066944b7011676181938e514a14
mustafe <mustafes900@hotmail.com>
2020-03-12 20:23:23 +0000

    refactored the index, to import the functions

--- src/index.js
+++ src/index.js
@@ -1,101 +1,17 @@
-import { fromEvent, of, from, combineLatest } from "rxjs";
-import { fromFetch } from "rxjs/fetch";
-import {
-  filter,
-  map,
-  debounceTime,
-  distinctUntilChanged,
-  switchMap,
-  catchError,
-  mergeMap
-} from "rxjs/operators";
+import {mergeMap} from "rxjs/operators";
+import {search} from "./search";
+import {seachInput} from "./searchInput";
+import {seachResult} from "./searchArea";
 
-const searchInput = document.querySelector("input");
+const inputElement = document.querySelector("input");
 
-let input = fromEvent(searchInput, "keyup").pipe(
-      filter(e => e.target.value.length > 2),
-      map(e => e.target.value),
-      debounceTime(600),
-      distinctUntilChanged()
-    );
+let youtubeSearch = seachInput(inputElement)
+            .pipe(mergeMap(x => search(x)));
 
-//TODO: Emit this event elsewhere to allow us to render the results
-
-
-let SearchInput = query =>  {
-  //TODO: Understand what mergeMap/SwitchMap/CatchError deeper
-  return fromFetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyCTH6Zu-mG4DtZ73JYtz0OH9uT5eLh-73w`
-  ).pipe(
-    switchMap(Response =>
-      Response.ok
-        ? from(Response.json())
-        : of({ error: true, message: `Error: ${Response.status}` })
-    ),
-    map(data => {
-      //TODO: Errors will stop the observable
-      const {
-        etag,
-        regionCode,
-        nextPageToken,
-        pageInfo: { totalResults, resultsPerPage },
-        items
-      } = data;
-      return {
-        etag,
-        regionCode,
-        nextPageToken,
-        pageInfo: {
-          totalResults,
-          resultsPerPage
-        },
-        items
-      };
-    }),
-    catchError(err => {
-      //network or other client error
-      console.log(err);
-      return of({ error: true, message: `Error ${err.message}` });
-    })
-  );
-};
-
-let CreateResultArea = data => {
-
-      let container = document.createElement("div");
-
-      container.classList = ["container"];
-
-      if(!data || !data.items){
-            return of(container);
-      }
-
-      for(let item of data.items){
-            
-            let result = document.createDocumentFragment("div");
-
-            console.log({item});
-
-            result.classList = ["video"];
-
-            container.appendChild(result);
-      }
-
-      return of(container);
-};
-
-//TODO: Construct this string yourself
-
-//TODO: ON input change react and update the search, take that input and listen to it
-
-let resultArea = document.querySelector("#result");
-
-let data = input.pipe(mergeMap(x => SearchInput(x)));
-
-let newDivElementOnUserSearch = data.pipe(mergeMap(x => CreateResultArea(x))).subscribe({
+let newDivElementOnUserSearch = youtubeSearch.pipe(mergeMap(x => seachResult(x))).subscribe({
       next: container => {
 
-            //TODO: on input search create a spinner, when data is loading keep the spinner! 
+            //TODO: on input search create a spinner, when youtubeSearch is loading keep the spinner! 
            let searchResultArea = document.getElementById("result");
 
            if(searchResultArea.childElementCount > 0 && container.childElementCount > 0){
